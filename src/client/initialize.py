c3851207243c14a0592dc83af4bb4dc336dd5a78
Maksim <maxmarinenkov68@gmail.com>
2022-11-26 01:28:05 +0300

    убрал лишнее

--- src/client/initialize.py
+++ src/client/initialize.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from settings.settings import TOKEN_CLIENT
 
@@ -8,4 +7,4 @@ TOKEN = TOKEN_CLIENT
 bot = Bot(TOKEN)
 storage = RedisStorage2(host='redis', port=6379, db=10)
 dp = Dispatcher(bot, storage=storage)
-scheduler = AsyncIOScheduler()
+
