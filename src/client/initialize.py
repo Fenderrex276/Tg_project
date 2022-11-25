01a7841145df142ae57007817c7e48eb1191b868
Maksim <maxmarinenkov68@gmail.com>
2022-11-26 00:13:26 +0300

    change token

--- src/client/initialize.py
+++ src/client/initialize.py
@@ -2,8 +2,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-TOKEN = ['5646356632:AAFmwBQuf7V91t--qGoJS4tcAdCCpNI8PAo']
-bot = Bot(TOKEN[0])
+TOKEN = '5646356632:AAFmwBQuf7V91t--qGoJS4tcAdCCpNI8PAo'
+bot = Bot(TOKEN)
 storage = RedisStorage2(host='redis', port=6379, db=10)
 dp = Dispatcher(bot, storage=storage)
 scheduler = AsyncIOScheduler()
