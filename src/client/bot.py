bbfc5c0c3358da6b943f531a187fcd87cb7f5f67
Maksim <maxmarinenkov68@gmail.com>
2022-11-27 23:55:24 +0300

    подгрузка периодических задач после запуска

--- src/client/bot.py
+++ src/client/bot.py
@@ -20,7 +20,7 @@ from branches.thirty_days_dispute.callbacks import register_callback as rc6
 from branches.training.branches import Training
 from branches.training.callbacks import register_callback as rc5
 from client.initialize import scheduler
-
+from client.tasks import load_periodic_tasks
 
 branches = [Start, DisputeWithFriend, ConfirmDispute, Pay, Training, CurrentDispute]
 callbacks = [rc1, rc2, rc3, rc4, rc5, rc6, rc7, rc8, rc9, rc10, rc11, rc12, rc13]
@@ -40,4 +40,5 @@ class DisputeBot:
 
     def start(self):
         scheduler.start()
+        load_periodic_tasks()
         executor.start_polling(self.dp, skip_updates=True)
